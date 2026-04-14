699f1d7c51e247177496c81fbfedb299f67e1a5c
pmayur1997 <mayurpatel9717@gmail.com>
2026-04-14 11:58:59 +0530

    fix: remove trailing slash from CORS origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",        # local backend
         "http://localhost:5173",        # vite preview
-        "https://student-api-production-5b6d.up.railway.app/",  # production Frontend
+        "https://student-api-production-5b6d.up.railway.app",  # production Frontend
         #"https://api-production-5b6d.up.railway.app/" # production Backend
     ],
     allow_credentials=True,
